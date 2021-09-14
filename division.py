728b049215f2e59affcfd568e5d0273ea9e0b1d3
RS-Rak <90445569+RS-Rak@users.noreply.github.com>
2021-09-14 11:46:43 +0100

    Delete division.py
    Moving the files to a different branch.

--- division.py
+++
@@ -1,7 +0,0 @@
-num = input("Please choose an integer and a divisor.")
-divisor = input()
-result = int(num)//int(divisor)
-remainder = int(num)%int(divisor)
-print(str(result) + ' remainder ' + str(remainder))
-
-#Divides a number and gives the remainder
