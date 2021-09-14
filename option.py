12c24f9ac0e34b100aee5fdd3f9f138c1cad535a
RS-Rak <90445569+RS-Rak@users.noreply.github.com>
2021-09-14 11:48:26 +0100

    Delete option.py

--- option.py
+++
@@ -1,6 +0,0 @@
-option = input("Please choose a number between 1 and 3.")
-while int(option) > 3 or int(option) < 1:
-    option = input("Invalid choice. Please choose a number between 1 and 3.")
-print("You have chosen option " + str(option))
-
-#Keep choosing a number till you choose 1 between 1 and 3
