c9edd8696de41ef1b888d747fc64ea2f8166bf29
RS-Rak <rulingshadows@gmail.com>
2021-09-17 15:14:13 +0100

    more tasks

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,10 @@
 print("Please input a piece of text you'd like to be encrypted")
 text = str(input()) #Takes the string being encrypted
 shift = 3 #How much the characters are getting shifted by
-encrypted = '' #The encrypted text
-for i in text:
+encrypted = [] #The encrypted text
+for i in range(len(text)):
     if i != ' ' or '.' or '?' or '!':
-        
+        encrypted.append(chr(ord(text[int(i)]) + shift)) 
+
+print(encrypted)
 #working on it
