f2def526a338392ab19eb2cd19c89c32930bbd18
RS-Rak <rulingshadows@gmail.com>
2021-09-17 14:50:39 +0100

    minor change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Yusuf Hassan's python repo
 Computer Science 2023 class
+
+This is where I've put all my python exercises for class. Enjoy. 
---
+++ caesar_cipher.py
@@ -0,0 +1,8 @@
+print("Please input a piece of text you'd like to be encrypted")
+text = str(input()) #Takes the string being encrypted
+shift = 3 #How much the characters are getting shifted by
+encrypted = '' #The encrypted text
+for i in text:
+    if i != ' ' or '.' or '?' or '!':
+        
+#working on it
