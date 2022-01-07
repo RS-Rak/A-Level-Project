8ad507036c9bf9bfaeda4bb2d7a6cb2173ccec3d
RS-Rak <90445569+RS-Rak@users.noreply.github.com>
2022-01-07 12:18:46 +0000

    Delete BasicExercises directory

--- BasicExercises/practice.py
+++
@@ -1,5 +0,0 @@
-def calculations(num1, num2):
-    sum = int(num1) + int(num2)
-    multiple = int(num1) * int(num2) 
-    return sum, multiple 
-sum, multiple = calculations((input("Please enter a number: ")), ((input("Please enter another number: "))))
